392ba6cae1d20718387961779a84ad25b3a992e9
nnaso0275q <nnaso0275@gmail.com>
2025-10-13 10:20:43 +0800

    skjhfd

--- app/api/categories/delete/route.ts
+++ app/api/categories/delete/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
-import { categories } from "../data";
+// import { categories } from "../data";
 
 export async function POST(req: Request) {
   const body = await req.json();
   const { category } = body;
 
-  const response = NextResponse.json({ data: categories}, { status: 200 });
+  const response = NextResponse.json({ data: categories }, { status: 200 });
   response.headers.set("Access-Control-Allow-Origin", "*"); // Or '*' for all origins
   response.headers.set(
     "Access-Control-Allow-Methods",
