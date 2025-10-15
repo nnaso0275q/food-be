7e1b48691adad284bbdcb88e7667cea5792a6ccb
nnaso0275q <nnaso0275@gmail.com>
2025-10-15 14:34:53 +0800

    jksdn

--- app/api/food/route.ts
+++ app/api/food/route.ts
@@ -1,82 +1,71 @@
-import { createFood, getAllFoods } from "@/lib/food/foodService"
+import { createFood, getAllFoods } from "@/lib/services/foodService";
 import { FoodType } from "@/lib/types/types";
+import { NextRequest, NextResponse } from "next/server";
 import { uploadImageToCloudinary } from "@/lib/utils/uploadimage";
-import { NextRequest, NextResponse } from "next/server"
 
-// export async function GET() {
-//   const foods = await getAllFoods();
-//   return Response.json({ data: foods });
-// }
+export const GET = async () => {
+  const foods = await getAllFoods();
+  return NextResponse.json({ message: "hello", data: foods }, { status: 200 });
+};
 
 export async function POST(request: NextRequest) {
-  
-    // Parse the formData from the request
-    const formData = await request.formData();
+  // Parse the formData from the request
+  const formData = await request.formData();
 
-    // Extract food fields from formdata
-    const name = formData.get("name") as string;
-    const ingredients = formData.get("ingredients") as string;
-    const price = formData.get("price") as string;
-    const categoryId = formData.get("categoryId") as string;
-    const image = formData.get("image") as File;
+  // Extract food fields from formdata
+  const name = formData.get("name") as string;
+  const ingredients = formData.get("ingredients") as string;
+  const price = formData.get("price") as string;
+  const categoryId = formData.get("categoryId") as string;
+  const image = formData.get("image") as File;
 
-    // Console log the received data
-    console.log("-----Received Food Data-----");
-    console.log("Name:", name);
-    console.log("Ingredients:", ingredients);
-    console.log("Price", price);
-    console.log("Category:", categoryId);
-    console.log(
-      "Image:",
-      image ? `${image.name} (${image.size} bytes)` : "No image"
-    );
-    console.log("----------------------------");
+  // Console log the received data
+  console.log("----------------Received Food Data----------------");
+  console.log("Name:", name);
+  console.log("Ingredients:", ingredients);
+  console.log("Price", price);
+  console.log("Category:", categoryId);
+  console.log(
+    "Image:",
+    image ? `${image.name} (${image.size} bytes)` : "No image"
+  );
+  console.log("-------------------------------------------------");
 
-    return NextResponse.json(
-      {message: "Food item received successfully"},
-      {status: 200}
-    )
+  const uploadedUrl = await uploadImageToCloudinary(image);
+  console.log(uploadedUrl);
 
-  //   // Validate required fields
-  //   if (!name || !ingredients || !price) {
-  //     return NextResponse.json(
-  //       { error: "Missing required fields" },
-  //       { status: 400 }
-  //     );
-  //   }
+  // Prepare data
+  const foodData: FoodType = {
+    name,
+    ingredients,
+    image: uploadedUrl,
+    price: parseFloat(price),
+    categoryId,
+  };
+  // Save to mongoDB
+  const result = await createFood(foodData);
 
-  //   // Handle image upload if image exists
-  //   let imageUrl = "";
-  //   if (image) {
-  //     imageUrl = await uploadImageToCloudinary(image);
-  //   }
-
-  //   // Prepare the food data object
-  //   const foodData: FoodType = {
-  //     name,
-  //     ingredients,
-  //     price: parseFloat(price),
-  //     categoryId,
-  //     image: imageUrl,
-  //   };
-  //   await createFood(foodData);
-  //   console.log("Final Food Data:", foodData);
-
-  //   // Return success response
-  //   return NextResponse.json(
-  //     {
-  //       success: true,
-  //       message: "Food item received and image uploaded successfully",
-  //       data: foodData,
-  //     },
-  //     { status: 201 }
-  //   );
- }
-
-
-
-export const GET =async()=>{
-  const foods = await getAllFoods()
-  console.log(foods)
-  return NextResponse.json({message: "hello"}, {status: 200})
+  return NextResponse.json(
+    { message: "Food item received successfully" },
+    { status: 200 }
+  );
 }
+
+//   // Validate required fields
+//   if (!name || !ingredients || !price) {
+//     return NextResponse.json(
+//       { error: "Missing required fields" },
+//       { status: 400 }
+//     );
+//   }
+
+//   // Return success response
+//   return NextResponse.json(
+//     {
+//       success: true,
+//       message: "Food item received and image uploaded successfully",
+//       data: foodData,
+//     },
+//     { status: 201 }
+//   );
+// }
--- lib/services/foodService.ts
+++ lib/services/foodService.ts
@@ -1,10 +1,16 @@
-
 import { Food } from "../models/Food";
 import { FoodType } from "../types/types";
 import connectDB from "../utils/mongodb";
 
-export const getAllFoods =async(): Promise<FoodType[]>=>{
-   await connectDB()
-   const allFoods=await Food.find({})
-   return allFoods
-}
+export const createFood = async (foodData: FoodType) => {
+  await connectDB();
+  const newFood = new Food(foodData);
+  await newFood.save();
+  return newFood;
+};
+
+export const getAllFoods = async (): Promise<FoodType[]> => {
+  await connectDB();
+  const allFoods = await Food.find({});
+  return allFoods;
+};
